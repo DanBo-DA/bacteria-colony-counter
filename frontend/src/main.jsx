2b46c54e4481fe83e21ee096bd23490880400247
Daniel Borges <lucasborges279@hotmail.com>
2025-05-31 23:00:01 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// Forçando novo deploy para quebrar cache persistente.
+console.log('Versão 2 do main.jsx carregada.');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
